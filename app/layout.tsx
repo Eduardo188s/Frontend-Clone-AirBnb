5d1b2f0c958331ec18148e8e9331fdfffa700d99
Eduardo188s <eduardodelarosa188@gmail.com>
2025-03-05 12:30:54 -0600

    13 Log in modal (remember error codes)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Content, Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
-import Modal from "./components/modals/Modal";
+import LoginModal from "./components/modals/LoginModal";
 
 const inter = Inter({subsets: ["latin"]})
 
@@ -31,6 +31,7 @@ export default function RootLayout({
           <div className="pt-24">
             {children}
           </div>
+          <LoginModal/>
       </body>
     </html>
   );
